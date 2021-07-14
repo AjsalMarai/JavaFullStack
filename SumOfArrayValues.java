ad7f98ffe28bc26a109098d9fc0cd4f6fce0239e
AjsalMarai <86570376+AjsalMarai@users.noreply.github.com>
2021-07-14 12:35:32 +0530

    Add files via upload

---
+++ SumOfArrayValues.java
@@ -0,0 +1,14 @@
+package labexercises;
+
+public class SumOfArrayValues {
+	public static void main(String[] args) {
+		int[] array = {10, 20, 30, 40, 50, 10};
+		int sum = 0;
+		
+		for(int num : array) {
+			sum = sum + num;
+		}
+		System.out.println("The sum of array elements is: "+sum);
+	}
+
+}
